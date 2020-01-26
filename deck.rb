218179ea583f53fff7c64cd571d28eb9a047ad61
Michael Zemel <michael.zemel@stitchfix.com>
2020-01-26 08:58:41 -0700

    Mostly completed prototype of resource-producing allies

--- deck.rb
+++ deck.rb
@@ -1,23 +1,51 @@
 require 'squib'
 require 'pry'
 
-Squib::Deck.new width: 825, height: 1125, cards: 15 do
-  data = xlsx file: 'data/allies/resource_allies.xlsx'
+BLOCK = 37.5
+COLORS = {
+  'R' => 'red',
+  'Y' => 'yellow',
+  'G' => 'green',
+  'B' => 'blue',
+  'N' => 'brown'
+}
 
-  png file: 'icons/ally.png'
-  text str: data["Name"], x: inches(1), y: inches(1)
+# Resource-producing allies
+Squib::Deck.new width: 825, height: 1125, cards: 15, layout: 'layout.yml' do
+  background color: '#ecc9a3'
+  rect layout: 'cut'
+  rect layout: 'bleed'
+  data = xlsx file: 'data/allies/resource_allies.xlsx', explode: 'Quantity'
+
+  svg file: 'icons/ally.svg', layout: 'icon'
 
   costs = data["Cost"].map {|str| str.split('').each_slice(2) }
   costs.each_with_index do |card_cost, card_number|
     card_cost.each_with_index do |(number, color), index|
-      png range: card_number, file: "icons/cubes/#{color}.png", x: inches(1) + inches(index)/2 
-      text range: card_number, str: number, x: inches(1.1) + inches(index)/2, y: inches(0.1)
+      png range: card_number,
+          file: "icons/cubes/#{color}.png",
+          layout: 'cube',
+          x: 8 * BLOCK + 3.5 * BLOCK * index,
+          y: 4 * BLOCK
+      text range: card_number,
+           str: number,
+           x: 9 * BLOCK  + 3.5 * BLOCK * index,
+           y: 4.5 * BLOCK 
     end
   end
 
-  png file: data["Color"].map {|c| "icons/cubes/#{c}.png"}, x: inches(1), y: inches(2)
+  rect layout: 'title'
+  text str: data["Name"], layout: 'title'
+
+  rect layout: 'art'
+  text str: 'Art', layout: 'art', y: 13 * BLOCK
+
+  rect layout: 'effect'
+  png file: data["Color"].map {|c| "icons/cubes/#{c}_full.png"}, layout: 'cube', x: 10 * BLOCK, y: 19 * BLOCK
   
-  # rider
-  # png file: data["Rider Cost"].map {|c| "icons/cubes/#{c}.png"}, x: inches(1), y: inches(3)
+  rect layout: 'alt'
+  text str: 'Trash this card for', layout: 'alt_text'
+  svg file: 'icons/shield.svg', layout: 'alt_shield'
+  text str: 1, layout: 'alt_points'
   save_png prefix: 'allies_'
 end
